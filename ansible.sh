3b4be537c09b6d9a3f8959f3e5f99a340a9931c0
zenmaster24 <zen.accounts@hotmail.com>
2018-05-31 21:40:54 +1000

    Added script to install ansible in a python venv, and then run ansible-pull on a git repo.

---
+++ ansible.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# stop on errors
+set -ex
+
+# install python3, pip and use pip to install virtual env
+sudo apt install -y python3 python3-pip python3-apt && sudo pip3 install virtualenv
+
+# set ansible environment name
+ansible=${1:-ansible}
+
+# set up a virtual env
+virtualenv --system-site-packages "${ansible}"
+# activate virtualenv
+source "./${ansible}/bin/activate"
+# install ansible
+pip install ansible
+
+# run ansible-pull
+ansible-pull --clean --full --purge --url git@github.com:zenmaster24/ansible.git --only-if-changed
